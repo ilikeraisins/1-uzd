dc8e596cd5e4c42d65e2007d0e5fce40bad86c39
unknown <monemuznikaite@gmail.com>
2025-11-05 22:56:26 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main() {
             string eil;
 
             timer t;
-            std::stringstream buffer = failo_skaitimas("1000studentai.txt");
+            std::stringstream buffer = failo_skaitimas("10000000studentai.txt");
             cout << "Failo nuskaitymas: " << t.elapsed() << endl;
           
             std::getline(buffer, eil);
